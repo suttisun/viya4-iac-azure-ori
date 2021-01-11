e0b26912ebb8602644e57a7acda4ca3f4f465ab2
Norman Johnson <norman.johnson@sas.com>
2021-01-11 18:01:56 -0500

    unpid version

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
     }
     azureread = {
       source  = "hashicorp/azuread"
-      version = ">= 1.1.1"
+      version = "~> 1.1.1"
     }
     external = {
       source  = "hashicorp/external"
